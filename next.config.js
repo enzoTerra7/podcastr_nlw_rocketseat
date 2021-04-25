28ef573be373dae122010642353ffeba42add32e
Enzo Terra <enzotrr@gmali.com>
2021-04-25 17:42:24 -0300

    PWA actualization

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
-module.exports= {
-    images: {
-        domains: ['storage.googleapis.com'],
-    }
-}
+const withPwa = require('next-pwa');
+const withPlugins = require('next-compose-plugins');
+
+module.exports = withPlugins([
+    {
+        images: {
+            domains: ['storage.googleapis.com']
+        },
+
+        distDir: 'build',
+    },
+    [withPwa, {
+        pwa: {
+            disable: process.env.NODE_ENV != 'production',
+            dest: 'public',
+            register: true,
+            sw: '/sw.js'
+        }
+    }]
+])
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,13 @@ export default class MyDocument extends Document {
         return(
             <Html>
                 <Head>
+                    <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
                     <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet"></link>
+                    <link rel="manifest" href="/manifest.json"/>
+                    <link rel="icon" href="/favicon.png"/>
+                    <link rel="shortcut icon" href="/icon-128x128.png"/>
                 </Head>
                 <body>
                     <Main />
